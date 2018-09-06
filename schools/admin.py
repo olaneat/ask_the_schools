bdb7db32a3c450c0867fb62be08f4a518b353415
olaneat <olaneat@yahoo.com>
2018-09-06 11:21:15 +0100

    update

--- schools/admin.py
+++ schools/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Schools, parent_signup
+from .models import Schools, parent_signup, parents_remark
 # Register your models here.
 
 
 @admin.register(Schools)
 class schoolsadmin(admin.ModelAdmin):
-	list_display = ('School_name', 'school_badge', 'short_video','address')
+	list_display = ('name', 'badge', 'fees', 'video', 'status',  'address')
 
 
 @admin.register(parent_signup)
@@ -13,3 +13,7 @@ class parent_signup (admin.ModelAdmin):
 	list_display = ('first_name', 'surname', 'email', 'phone_no')
 	fields = ('surname', 'first_name', 'phone_no', 'email')
 
+
+@admin.register(parents_remark)
+class parents_remark(admin.ModelAdmin):
+	list_display =('full_name', 'school_name', 'comment')
--- schools/models.py
+++ schools/models.py
@@ -7,18 +7,21 @@ from django.urls import reverse
 
 
 class Schools(models.Model):
-	School_name = models.CharField(max_length = 300, help_text  = "fill in the name of the school below")
+	name = models.CharField(max_length = 300, help_text  = "fill in the name of the school below")
 	address = models.CharField(max_length  = 250 )
-	school_badge = models.FileField(upload_to = "media/images", default = True, help_text = "upload a jpg file ")
-	school_info = models.TextField(max_length = 1000)
-	short_video = models.FileField(upload_to = 'media/video', default = True, help_text = "upload a video file, mp4, " )	
+	badge = models.FileField(upload_to = "media/images", null = True, blank= True, help_text = "upload a jpg file ")
+	info = models.TextField(max_length = 1000)
+	video = models.FileField(upload_to = 'media/video', null = True, blank= True, help_text = "upload a video file, mp4, " )	
+	status = models.CharField( max_length = 30, help_text ="kindly enter the school type i.e Boarding or Day", null =True)
+	fees = models.CharField(max_length = 70, help_text = 'kindly enter the appropriate fees', null = True)
+	
 
 
 	def get_absolute_url(self):
 		return reverse ('school-detail', args = [str(self.id)])		
 
 class parent_signup(models.Model):
-	first_name = models.CharField(max_length = 100, help_text = "enter your first name" )
+	first_name = models.CharField(max_length = 200, help_text = "enter your first name" )
 	surname = models.CharField(max_length = 150, help_text = "enter your surname")
 	bio = models.TextField(max_length = 1000)
 	dirth_of_birth = models.CharField(max_length = 15, help_text = "please enter a valid birth date YYYY-MM-DD")
@@ -32,7 +35,7 @@ class parent_signup(models.Model):
 		return '{0}, {1} '.format(self.surname, self.first_name) 
 
 class parents_remark(models.Model):
-	parents_name = models.CharField(max_length = 300, help_text = "fill in your full name" )
-	School_in_review_name= models.CharField(max_length = 150, help_text = "fill in the school name" )
+	full_name = models.CharField(max_length = 300, help_text = "fill in your full name" )
+	school_name= models.CharField(max_length =100 , null = True  )
 	comment = models.TextField(max_length =1000 )
 	
